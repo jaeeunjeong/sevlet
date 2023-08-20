209a9cbf940cf68ce2f627d6aefba999a88eb5ab
jaeeunjeong <jejeong000@gmail.com>
2023-08-20 19:52:14 +0900

    [add]스프링 MVC- 구조 이해 실습

---
+++ springmvc/src/main/java/hello/springmvc/basic/requsetmapping/MappingController.java
@@ -0,0 +1,57 @@
+package hello.springmvc.basic.requsetmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MappingController {
+
+  private Logger log = LoggerFactory.getLogger(getClass());
+
+  @RequestMapping("/hello-basic")
+  public String helloBasic() {
+    log.info("helloBasic");
+    return "ok";
+  }
+
+  @GetMapping("/mapping/{userId}")
+  public String mappingPath(@PathVariable("userId") String data) {
+    log.info("mappingPath userId={}", data);
+    return "ok";
+  }
+
+  @GetMapping("/mapping/users/{userId}/orders/{orderId}")
+  public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
+    log.info("userId = {}, orderId= {}", userId, orderId);
+    return "ok";
+  }
+
+  @GetMapping(value = "/mapping-param", params = "mode=debug")
+  public String mappingParam() {
+    log.info("mappingParam");
+    return "ok";
+  }
+
+  @GetMapping(value = "/mapping-header", headers = "mode=debug")
+  public String mappingHeader() {
+    log.info("mappingHeader");
+    return "ok";
+  }
+
+  @PostMapping(value = "/mapping-consume", consumes = "application/json")
+  public String mappingConsumes() {
+    log.info("mappingConsumes");
+    return "ok";
+  }
+
+  @PostMapping(value = "/mapping-produce", produces = "text/html")
+  public String mappingProduces() {
+    log.info("mappingProduce");
+    return "ok";
+  }
+}
